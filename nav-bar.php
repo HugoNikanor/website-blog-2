b589954aeceab59da82500c09692659c6e291ee5
Hugo Hörnquist <hugo@hornquist.se>
2018-01-04 01:06:45 +0100

    Moved entry list loading into own file

--- nav-bar.php
+++ nav-bar.php
@@ -1,23 +1,6 @@
 <?php
-// load in all files in ./entries into an array
 
-// TODO safegaurd if there is no entries directory
-$entriesDir = './entries';
-foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($entriesDir)) as $fname)
-{
-	// filter out "." and "..", as well as vim tmp files
-	if ($fname->isDir()) continue;
-	if (substr($fname, strlen($fname) - 4, 4) === '.swp') continue;
-	if (substr($fname, strlen($fname) - 1, 1) === '~') continue;
-	if(substr($fname, strlen($entriesDir) + 1, 1) === '.') continue;
-	
-	$entries[] = substr($fname, 10);
-}
-
-
-// sort the entries
-natsort($entries);
-$entries = array_values($entries);
+require ("./load-entries.php");
 
 function get_nav_links() {
 	global $entries, $filename;
