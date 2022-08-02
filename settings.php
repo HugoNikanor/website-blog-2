14aa11c294af1f45648808d95dacd431b5c1bdc1
Hugo Hörnquist <hugo@lysator.liu.se>
2022-08-03 00:26:12 +0200

    Document some settings.

--- settings.php
+++ settings.php
@@ -4,5 +4,5 @@ $blog_title = "HugoNikanors blogg‽";
 $blog_subtitle = "A blog about nothing, but mostly itself.";
 $http_host = $_SERVER["HTTP_HOST"];
 $urlbase = "http://$http_host/hugo";
-$has_comments = false;
-$has_entry_log = true;
+$has_comments = false; // Should a comment section exist?
+$has_entry_log = true; // Should each entries changelog be shown (if available)
