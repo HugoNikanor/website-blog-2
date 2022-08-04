cf63e2c6066c91a47ec466f8eb196ccd77245c5d
Hugo Hörnquist <hugo@lysator.liu.se>
2022-08-04 04:11:03 +0200

    Split settings file into default and user override.

---
+++ default_settings.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * DO NOT EDIT
+ * These are the default fallbacks for the site. Removing anything
+ * here will cause the code to fail due to missing variables.
+ *
+ * User edits should instead be done in settings.php.
+ *
+ * Default values are subject to change.
+ */
+$author = "Firsname Lastnameson";
+$blog_title = "Default blog!";
+$blog_subtitle = "Which should probably be configured.";
+$http_host = $_SERVER["HTTP_HOST"];
+$urlbase = "http://$http_host";
+$has_comments = false; // Should a comment section exist?
+$has_entry_log = true; // Should each entries changelog be shown (if available)
--- settings.php
+++ settings.php
@@ -1,8 +1,14 @@
 <?php
-$author = "Hugo Hornquist";
-$blog_title = "HugoNikanors blogg‽";
-$blog_subtitle = "A blog about nothing, but mostly itself.";
-$http_host = $_SERVER["HTTP_HOST"];
+require('./default_settings.php');
+/*
+ * User overridable settings.
+ *
+ * The normative list of all settings, including their default values
+ * is in default_settings.php.
+ *
+ * Default values are subject to change.
+ */
+$author = "Hugo Hörnquist";
+$blog_title = "Hugo's Blog";
+$blog_subtitle = "A blog about writing a blog engine";
 $urlbase = "http://$http_host/hugo";
-$has_comments = false; // Should a comment section exist?
-$has_entry_log = true; // Should each entries changelog be shown (if available)
